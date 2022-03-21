1bb45a1b2333147ab3fcd67e94941434bc81745e
Pratyush Manav <pratyush.manav@in.abb.com>
2022-03-21 14:29:37 +0530

    state and setState

--- src/App.js
+++ src/App.js
@@ -4,22 +4,14 @@ import Greet from './components/Greet';
 import { Welcome } from './components/Welcome'
 import Hello from './components/Hello';
 import HelloWithoutJSX from './components/HelloWithoutJSX';
+import Message from './components/Message';
+import Counter from './components/Counter';
 
 function App() {
   return (
     <div className="App">
-      <Greet name="Bruce" heroName="Batman">
-        <p>This is children props.</p>
-      </Greet>
-      <Greet name="Clark" heroName="Superman" >
-        <button>Action</button>
-      </Greet>
-      <Greet name="Diana" heroName="Wonder Woman" ></Greet>
-      <Welcome name="Bruce" heroName="Batman" />
-      <Welcome name="Clark" heroName="Superman" />
-      <Welcome name="Diana" heroName="Wonder Woman" />
-      <Hello />
-      <HelloWithoutJSX />
+      <Message />
+      <Counter />
     </div>
   );
 }
