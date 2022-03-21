a93ac173a0d66f541d2a2195677eba2a24f19caf
Pratyush Manav <pratyush.manav@in.abb.com>
2022-03-21 18:41:51 +0530

    Event Handeling

--- src/App.js
+++ src/App.js
@@ -6,10 +6,14 @@ import Hello from './components/Hello';
 import HelloWithoutJSX from './components/HelloWithoutJSX';
 import Message from './components/Message';
 import Counter from './components/Counter';
+import FunctionClick from './components/FunctionClick';
+import ClassClick from './components/ClassClick';
 
 function App() {
   return (
     <div className="App">
+      <FunctionClick />
+      <ClassClick />
       <Message />
       <Counter />
     </div>
