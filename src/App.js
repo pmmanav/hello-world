1d67e3e270ea16fe0c29756bfa7f213297995654
Pratyush Manav <pratyush.manav@in.abb.com>
2022-03-22 11:08:58 +0530

    Event Binding

--- src/App.js
+++ src/App.js
@@ -8,14 +8,12 @@ import Message from './components/Message';
 import Counter from './components/Counter';
 import FunctionClick from './components/FunctionClick';
 import ClassClick from './components/ClassClick';
+import EventBind from './components/EventBind';
 
 function App() {
   return (
     <div className="App">
-      <FunctionClick />
-      <ClassClick />
-      <Message />
-      <Counter />
+      <EventBind />
     </div>
   );
 }
