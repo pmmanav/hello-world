02c899f7d7cc5396694685dfae74226f8f11d68e
Pratyush Manav <pratyush.manav@in.abb.com>
2022-03-23 12:41:53 +0530

    Conditional Rendering

--- src/components/UserGreeting.js
+++ src/components/UserGreeting.js
@@ -6,23 +6,41 @@ class UserGreeting extends Component {
         super(props)
       
         this.state = {
-           isLoggedIn: false
+           isLoggedIn: false 
         }
       }
   render() {
-      if(this.state.isLoggedIn){
-          return(
-              <div>
-                  Welcome, Pratyush Mani Manav
-              </div>
-            )
-      } else {
-          return (
-              <div>
-                  Welcome, Guest
-              </div>
-          )
-      }
+
+    return this.state.isLoggedIn && <div>Welcome, Pratyush Mani Manav</div>
+
+    // return (
+    //     this.state.isLoggedIn ?
+    //     ( <div>Welcome, Pratyush Mani Manav</div> ) :
+    //     ( <div>Welcome, Guest</div> )
+    // )
+
+    // let message
+    // if(this.state.isLoggedIn){
+    //     message = <div>Welcome, Pratyush Mnai Manav</div>
+    // } else {
+    //     message = <div>Welcome, Guest</div>
+    // }
+
+    // return <div>{message}</div>
+
+    //   if(this.state.isLoggedIn){
+    //       return(
+    //           <div>
+    //               Welcome, Pratyush Mani Manav
+    //           </div>
+    //         )
+    //   } else {
+    //       return (
+    //           <div>
+    //               Welcome, Guest
+    //           </div>
+    //       )
+    //   }
     // return (
     //   <div>
     //       <div>Welcome, Pratyush Mani Manav</div>
