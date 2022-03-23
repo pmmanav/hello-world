c2d81a3728ebb2b33cb51fb352f4ff4a5b05582c
Pratyush Manav <pratyush.manav@in.abb.com>
2022-03-23 21:55:31 +0530

    Styling & CSS Basics

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-import Greet from './components/Greet';
+import logo from './logo.svg'
+import './App.css'
+import Greet from './components/Greet'
 import { Welcome } from './components/Welcome'
-import Hello from './components/Hello';
-import HelloWithoutJSX from './components/HelloWithoutJSX';
-import Message from './components/Message';
-import Counter from './components/Counter';
-import FunctionClick from './components/FunctionClick';
-import ClassClick from './components/ClassClick';
-import EventBind from './components/EventBind';
-import ParentComponent from './components/ParentComponent';
-import UserGreeting from './components/UserGreeting';
-import NameList from './components/NameList';
+import Hello from './components/Hello'
+import HelloWithoutJSX from './components/HelloWithoutJSX'
+import Message from './components/Message'
+import Counter from './components/Counter'
+import FunctionClick from './components/FunctionClick'
+import ClassClick from './components/ClassClick'
+import EventBind from './components/EventBind'
+import ParentComponent from './components/ParentComponent'
+import UserGreeting from './components/UserGreeting'
+import NameList from './components/NameList'
+import Stylesheet from './components/Stylesheet'
+import Inline from './components/Inline'
+import './appStyles.css'
+import styles from './appStyles.module.css'
 
 function App() {
   return (
     <div className="App">
-      <NameList />
+      <h1 className='error'>Error</h1>
+      <h1 className = {styles.success}>Success</h1>
+      <Stylesheet primary = {true} />
+      <Inline />
+      {/* <NameList /> */}
       {/* <UserGreeting /> */}
       {/* <ParentComponent /> */}
       {/* <EventBind /> */}
